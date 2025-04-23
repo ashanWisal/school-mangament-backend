a359cacd09c8a6f35a9ee58f3c9e744f01cf3e4e
ashanWisal <ashanwisal08@gmail.com>
2025-04-24 01:02:34 +0500

    Create schemas and models

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,15 @@ import { CoursesController } from './courses/courses.controller';
 import { TeacherModule } from './teacher/teacher.module';
 import { TeacherService } from './teacher/teacher.service';
 import { StudentModule } from './student/student.module';
-import { StudentController } from './student/student.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [StudentModule, TeacherModule, CoursesModule],
+  imports: [StudentModule, TeacherModule, CoursesModule,
+    MongooseModule.forRoot('mongodb+srv://ashanwisal08:root@cluster0.tvzjfz0.mongodb.net/school')
+  ],
   controllers: [AppController, StudentController, CoursesController],
   providers: [AppService, TeacherService],
+  
 })
 export class AppModule {}
---
+++ src/teacher/Entities/teacher.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import * as mongoose from "mongoose";
+import { Document } from "mongoose"; 
+
+export type TeacherDocument = Teacher & Document; // define the type of the document
+
+@Schema()
+export class Teacher {
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    email: string;
+
+    @Prop({ required: true })
+    subject: string;
+
+    @Prop({ required: true })
+    createdAt: Date;
+
+    @Prop({type:[{type: mongoose.Schema.Types.ObjectId, ref: "Student"}]})
+    studentIds: mongoose.Schema.Types.ObjectId[]; // array of student ids
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: "Courses"})
+    courseId: mongoose.Schema.Types.ObjectId; 
+
+}
+
+export const TeacherSchema = SchemaFactory.createForClass(Teacher); // create the schema for the teacher model
