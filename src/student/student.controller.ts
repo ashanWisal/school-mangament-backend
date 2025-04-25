f10093593d5ae4971d763c9616624f010c2e52bd
ashanWisal <ashanwisal08@gmail.com>
2025-04-25 14:50:40 +0500

    Post method created for the student

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { CreateStudentDto } from './Dtos/student.dto';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor (private readonly studentService: StudentService){}
+
+    @Post()
+    async create(@Body() user: CreateStudentDto){
+        return this.studentService.createStudent(user)
+    }
+}
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
+import { StudentModel } from './Entities/student.model';
+import { StudentController } from './student.controller';
 
 @Module({
-  providers: [StudentService]
+  imports: [StudentModel],
+  controllers: [StudentController],
+  providers: [StudentService],
+  exports: [StudentService]
 })
 export class StudentModule {}
